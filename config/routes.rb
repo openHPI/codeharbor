ef9002aaac4369575b0c2c30700ed951649fc8f5
marcel <marcel@jankrift.de>
2015-12-09 15:21:11 +0100

    scss files restored

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   resources :exercises
+  resources :exercises
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'exercises#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
