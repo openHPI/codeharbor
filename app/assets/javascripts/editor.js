3d3dadf63d31a1bb6c5dfac5baab397effff3509
Marcel Jankrift <marcel@jankrift.de>
2016-01-30 21:04:02 +0100

    edit exercise files with ace working

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -6,6 +6,8 @@ ready = function() {
 
   for (var i=0, max=all.length; i < max; i++) {
     var elem = all[i].getElementsByClassName("editor")[0];
+    var hidden = all[i].getElementsByClassName("hidden")[0];
+
     var editor = ace.edit(elem);
     editor.setTheme("ace/theme/monokai");
     editor.getSession().setMode("ace/mode/java");
@@ -13,8 +15,7 @@ ready = function() {
     elem.style.fontSize='18px';
 
     editor.getSession().on('change', function(e) {
-      alert(editor.getValue());
-    // e.type, etc
+      hidden.value = editor.getValue();
     });
   }
 
