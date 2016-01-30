e8eaeaa3171401151ef3d5794c2ce70d776750e3
Marcel Jankrift <marcel@jankrift.de>
2016-01-30 20:58:09 +0100

    listen ace editor

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -1,14 +1,24 @@
-$(function() {
+var ready;
+ready = function() {
 
   ace.config.set('basePath', '/assets/ace/');
-  var all = document.getElementsByClassName("editor");
+  var all = document.getElementsByClassName("edit");
 
   for (var i=0, max=all.length; i < max; i++) {
-    var editor = ace.edit(all[i]);
+    var elem = all[i].getElementsByClassName("editor")[0];
+    var editor = ace.edit(elem);
     editor.setTheme("ace/theme/monokai");
     editor.getSession().setMode("ace/mode/java");
-    editor.setReadOnly(true);
-    all[i].style.fontSize='18px';
+    //editor.setReadOnly(true);
+    elem.style.fontSize='18px';
+
+    editor.getSession().on('change', function(e) {
+      alert(editor.getValue());
+    // e.type, etc
+    });
   }
 
-});
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
