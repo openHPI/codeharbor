7971b118d3f17822ec00a16aa6af5e741bae1714
Theresa <theresa.zobel@student.hpi.de>
2016-02-02 10:00:26 +0100

    Add search for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -72,6 +72,10 @@ class CommentsController < ApplicationController
     end
   end
 
+  def index
+    @comment = Comment.search(params[:search]).paginate(per_page: 5, page: params[:page])
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_comment
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,13 @@
 class Comment < ActiveRecord::Base
   belongs_to :exercise
   belongs_to :user
+
+
+  def self.search(search)
+    if search
+      where('text LIKE ?', "%#{search}%")
+    else
+      all
+    end
+  end
 end
