34e22563e751c3c382f3caa3a0cc692d131c60e6
Tom Staubitz <thomas.staubitz@hpi.uni-potsdam.de>
2017-07-06 21:15:50 +0200

    enabled better errors for production and development. Do not forget to disable it for production again before we go anywhere near live

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  
+  ### !!! Important: remove this line before this goes anywhere near productive !!!
+  config.consider_all_requests_local = true
 
   # Code is not reloaded between requests.
   config.cache_classes = true
