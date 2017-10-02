eccb8e35d1a978417f696a06673a2b4c9393a97d
Adrian Steppat <adrian.steppat@student.hpi.de>
2017-10-02 17:48:34 +0200

    Removed avg_rating from tests

--- spec/factories/exercise.rb
+++ spec/factories/exercise.rb
@@ -1,19 +1,16 @@
 FactoryGirl.define do
   factory :simple_exercise, class: 'Exercise' do
     sequence(:title) {|n| "Test Exercise #{n}" }
-    avg_rating 0
   end
 
   factory :exercise_with_author, class: 'Exercise' do
     title 'Some Exercise'
     authors {[FactoryGirl.create(:user), FactoryGirl.create(:user)]}
-    avg_rating 0
   end
 
   factory :only_meta_data, class: 'Exercise' do
   	title 'Some Exercise'
     maxrating 10
-    avg_rating 0
     private false
     authors {[FactoryGirl.create(:user), FactoryGirl.create(:user)]}
     execution_environment {FactoryGirl.create(:java_8_execution_environment)}
@@ -24,7 +21,6 @@ FactoryGirl.define do
 
   factory :exercise_with_single_java_main_file, class: 'Exercise' do
     title 'Some Exercise'
-    avg_rating 0
     execution_environment { FactoryGirl.create(:java_8_execution_environment) }
     after(:create) do |exercise|
       create(:simple_description, exercise: exercise)
@@ -34,7 +30,6 @@ FactoryGirl.define do
 
   factory :exercise_with_single_junit_test, class: 'Exercise' do
     title 'Exercises with single JUnit Test'
-    avg_rating 0
     execution_environment { FactoryGirl.create(:java_8_execution_environment) }
     after(:create) do |exercise|
       create(:simple_description, exercise: exercise)
@@ -44,7 +39,6 @@ FactoryGirl.define do
 
   factory :exercise_with_single_model_solution, class: 'Exercise' do
     title 'Exercises with single Model Solution'
-    avg_rating 0
     execution_environment { FactoryGirl.create(:java_8_execution_environment) }
     after(:create) do |exercise|
       create(:simple_description, exercise: exercise)
