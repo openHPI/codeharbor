fd39e084dfaccbb580f19450d133d9709162819e
Matthias Wiesner <matthias.wiesner@googlemail.com>
2017-10-20 11:01:10 +0200

    Fix action mailer config

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,18 +2,31 @@ options = YAML.load_file(Rails.root.join('config', 'action_mailer.yml'))[Rails.e
 
 Rails.configuration.action_mailer.delivery_method = options.delete("delivery_method")
 
-Rails.configuration.action_mailer[:smtp_settings] = {}
-Rails.configuration.action_mailer[:default_options] = {}
-Rails.configuration.action_mailer[:sendmail_settings] = {}
 
-options["smtp_settings"].each do |name, value|
-  Rails.configuration.action_mailer[:smtp_settings][name.to_sym] = value
-end unless options.nil?
+if options.key? "smtp_settings"
+  Rails.configuration.action_mailer[:smtp_settings] = {}
+  options["smtp_settings"].each do |name, value|
+    Rails.configuration.action_mailer[:smtp_settings][name.to_sym] = value
+  end unless options.nil?
+end
 
-options["default_options"].each do |name, value|
-  Rails.configuration.action_mailer[:default_options][name.to_sym] = value
-end unless options.nil?
+if options.key? "default_options"
+  Rails.configuration.action_mailer[:default_options] = {}
+  options["default_options"].each do |name, value|
+    Rails.configuration.action_mailer[:default_options][name.to_sym] = value
+  end unless options.nil?
+end
 
-options["sendmail_settings"].each do |name, value|
-  Rails.configuration.action_mailer[:sendmail_settings][name.to_sym] = value
-end unless options.nil?
+if options.key? "sendmail_settings"
+  Rails.configuration.action_mailer[:sendmail_settings] = {}
+  options["sendmail_settings"].each do |name, value|
+    Rails.configuration.action_mailer[:sendmail_settings][name.to_sym] = value
+  end unless options.nil?
+end
+
+if options.key? "default_url_options"
+  Rails.configuration.action_mailer[:default_url_options] = {}
+  options["default_url_options"].each do |name, value|
+    Rails.configuration.action_mailer[:default_url_options][name.to_sym] = value
+  end unless options.nil?
+end
