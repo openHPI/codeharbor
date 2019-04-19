d30aa9322fb2791547a8efc7773f3944ff8f017c
Karol <git@koehn.pro>
2019-04-19 12:33:28 +0200

    fix direct assignment

--- spec/views/file_types/edit.html.slim_spec.rb
+++ spec/views/file_types/edit.html.slim_spec.rb
@@ -3,8 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe 'file_types/edit', type: :view do
+  let!(:file_type) { assign(:file_type, FileType.create!) }
+
   it 'renders the edit file_type form' do
-    file_type = assign(:file_type, FileType.create!)
     render
 
     assert_select 'form[action=?][method=?]', file_type_path(file_type), 'post' do
--- spec/views/licenses/edit.html.slim_spec.rb
+++ spec/views/licenses/edit.html.slim_spec.rb
@@ -3,8 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe 'licenses/edit', type: :view do
+  let!(:license) { assign(:license, License.create!) }
+
   it 'renders the edit license form' do
-    license = assign(:license, License.create!)
     render
     assert_select 'form[action=?][method=?]', license_path(license), 'post' do
     end
