f77e4f61e958314989fff9d7e4aa68f0195c403e
Karol <git@koehn.pro>
2019-05-05 15:20:51 +0200

    add test and model-solutions support

--- app/services/proforma_service/export.rb
+++ app/services/proforma_service/export.rb
@@ -19,11 +19,11 @@ module ProformaService
         internal_description: @exercise.instruction,
         proglang: proglang,
         files: files,
-        # tests: '',
+        tests: tests,
         uuid: @exercise.uuid,
         parent_uuid: @exercise.clone_relations.first&.origin&.uuid,
-        language: @exercise.descriptions.first.language
-        # model_solutions: ''
+        language: @exercise.descriptions.first.language,
+        model_solutions: model_solutions
       )
     end
 
@@ -31,8 +31,60 @@ module ProformaService
       {name: @exercise.execution_environment.language, version: @exercise.execution_environment.version}
     end
 
+    def model_solutions
+      @exercise.exercise_files.filter { |file| file.role == 'Reference Implementation' }.map do |file|
+        # attr_accessor :id, :files, :description, :internal_description
+
+        Proforma::ModelSolution.new(
+          id: "ms-#{file.id}",
+          files: [
+            Proforma::TaskFile.new(
+              id: file.id,
+              content: file.content,
+              filename: file.full_file_name,
+              used_by_grader: false,
+              usage_by_lms: 'display',
+              visible: 'delayed',
+              binary: false
+            )
+          ]
+        )
+      end
+    end
+
+    def tests
+      @exercise.tests.map do |test|
+        # :id, :title, :description, :internal_description, :test_type, :files, :meta_data
+
+        Proforma::Test.new(
+          id: test.id,
+          title: test.exercise_file.name,
+          # description: '',
+          # internal_description: '',
+          # test_type: '',
+          files: test_file(test.exercise_file),
+          meta_data: {
+            'feedback-message' => test.feedback_message,
+            'testing-framework' => test.testing_framework.name,
+            'testing-framework-version' => test.testing_framework.version
+          }.compact
+        )
+      end
+    end
+
+    def test_file(file)
+      [Proforma::TaskFile.new(
+        id: file.id,
+        content: file.content,
+        filename: file.full_file_name,
+        used_by_grader: true,
+        visible: file.hidden ? 'no' : 'yes',
+        binary: false
+      )]
+    end
+
     def files
-      @exercise.exercise_files.map do |file|
+      @exercise.exercise_files.filter { |file| file.role != 'Reference Implementation' }.map do |file|
         if file.attachment.exists?
           bin_content = Paperclip.io_adapters.for(file.attachment).read
           Proforma::TaskFile.new(
