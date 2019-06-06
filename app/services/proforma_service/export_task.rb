97a45687f61646f27b1593915445050bc7af9156
Karol <git@koehn.pro>
2019-06-06 17:37:38 +0200

    fix rubocop complaints

--- app/services/proforma_service/export_task.rb
+++ app/services/proforma_service/export_task.rb
@@ -17,16 +17,24 @@ module ProformaService
     def create_task
       @task = Proforma::Task.new(
         title: @exercise.title,
-        description: @exercise.descriptions.first.text,
+        description: first_description,
         internal_description: @exercise.instruction,
         proglang: proglang,
-        files: files,
+        files: task_files,
         tests: tests,
         uuid: @exercise.uuid,
-        parent_uuid: @exercise.clone_relations.first&.origin&.uuid,
+        parent_uuid: parent_uuid,
         language: @exercise.descriptions.first.language,
         model_solutions: model_solutions
       )
+    end
+
+    def parent_uuid
+      @exercise.clone_relations.first&.origin&.uuid
+    end
+
+    def first_description
+      @exercise.descriptions.first.text
     end
 
     def proglang
@@ -80,35 +88,38 @@ module ProformaService
       )]
     end
 
-    def files
+    def task_files
       @exercise.exercise_files
                .filter { |file| file.role != 'Reference Implementation' && !file.in?(@exercise.tests.map(&:exercise_file)) }.map do |file|
-        if file.attachment.exists?
-          bin_content = Paperclip.io_adapters.for(file.attachment).read
-          Proforma::TaskFile.new(
-            id: file.id,
-            content: bin_content,
-            filename: file.full_file_name,
-            used_by_grader: false,
-            usage_by_lms: file.read_only ? 'display' : 'edit',
-            visible: file.hidden ? 'no' : 'yes',
-            binary: true,
-            internal_description: file.role || 'Regular File',
-            mimetype: file.attachment_content_type
-          )
-        else
-          Proforma::TaskFile.new(
-            id: file.id,
-            content: file.content,
-            filename: file.full_file_name,
-            used_by_grader: true,
-            usage_by_lms: file.read_only ? 'display' : 'edit',
-            visible: file.hidden ? 'no' : 'yes',
-            binary: false,
-            internal_description: file.role || 'Regular File'
-          )
-        end
+        task_file(file)
       end
+    end
+
+    def task_file(file)
+      Proforma::TaskFile.new(
+        {
+          id: file.id,
+          filename: file.full_file_name,
+          usage_by_lms: file.read_only ? 'display' : 'edit',
+          visible: file.hidden ? 'no' : 'yes',
+          internal_description: file.role || 'Regular File'
+        }.tap do |params|
+          if file.attachment.exists?
+            params[:content] = attachment_content(file)
+            params[:used_by_grader] = false
+            params[:binary] = true
+            params[:mimetype] = file.attachment_content_type
+          else
+            params[:content] = file.content
+            params[:used_by_grader] = true
+            params[:binary] = false
+          end
+        end
+      )
+    end
+
+    def attachment_content(file)
+      Paperclip.io_adapters.for(file.attachment).read
     end
   end
 end
