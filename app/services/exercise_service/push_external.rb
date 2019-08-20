543a28a2ba29b001686a2eb759adfd688c4fb539
Karol <git@koehn.pro>
2019-08-20 18:37:44 +0200

    add required headers

--- app/services/exercise_service/push_external.rb
+++ app/services/exercise_service/push_external.rb
@@ -11,8 +11,13 @@ module ExerciseService
       oauth2_client = OAuth2::Client.new(@account_link.client_id, @account_link.client_secret, site: @account_link.push_url)
       oauth2_token = @account_link[:oauth2_token]
       token = OAuth2::AccessToken.from_hash(oauth2_client, access_token: oauth2_token)
+      body = @zip.string
       begin
-        token.post(@account_link.push_url, body: @zip, headers: {'Content-Type' => 'application/zip'})
+        token.post(
+          @account_link.push_url,
+          body: body,
+          headers: {'Content-Type' => 'application/zip', 'Content-Length' => body.length.to_s, 'Accept' => 'application/json'}
+        )
         return nil
       rescue StandardError => e
         return e
