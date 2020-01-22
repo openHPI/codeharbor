c75388a0a32f8b9a4af76f5f1ebf9cc339202595
Karol <git@koehn.pro>
2020-01-22 16:57:23 +0100

    fix rubocop issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,7 +117,7 @@ Rails.application.configure do
   # which to precompile
   # , '*.woff', '*.ttf', '*.svg', '*.eot', '.woff2', '*.otf'
   # FixMe: Check styling and what is needed for production
-  config.assets.precompile = ['*.js', '*.css.erb', '*.coffee', '*.scss']
+  # config.assets.precompile = ['*.js', '*.css.erb', '*.coffee', '*.scss']
 
   # use relative URL path while compiling, maybe?
   config.assets.initialize_on_precompile = true
