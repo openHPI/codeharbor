7050ed876d83addc1a04b43011490d1b359b8adf
Karol <git@koehn.pro>
2020-02-11 17:23:20 +0100

    add specs

--- spec/models/group_membership_spec.rb
+++ spec/models/group_membership_spec.rb
@@ -6,35 +6,47 @@ RSpec.describe GroupMembership, type: :model do
   describe 'validations' do
     let(:group) { create(:group) }
     let(:member) { create(:user) }
-    let(:membership_type) { 'member' }
-    let(:group_membership) { build(:group_membership, member: member, group: group, membership_type: membership_type) }
+    let(:type) { 'member' }
+    let(:group_membership) { build(:group_membership, member: member, group: group, membership_type: type) }
 
     it 'is valid' do
       expect(group_membership).to be_valid
     end
 
     context 'when a group_membership with the same values already exists' do
-      before { create(:group_membership, member: member, group: group, membership_type: membership_type) }
+      let(:group_copy) { group }
+      let(:member_copy) { member }
+      let(:type_copy) { type }
+
+      before { create(:group_membership, member: member_copy, group: group_copy, membership_type: type_copy) }
 
       it 'is not valid' do
         expect(group_membership).not_to be_valid
       end
 
       context 'when member is different' do
-        let(:group) { create(:group) }
+        let(:member_copy) { create(:user) }
 
-        let(:member) { create(:user) }
+        it 'is valid' do
+          expect(group_membership).to be_valid
+        end
+      end
 
-        let(:membership_type) { 'member' }
+      context 'when group is different' do
+        let(:group_copy) { create(:group) }
 
+        it 'is valid' do
+          expect(group_membership).to be_valid
+        end
+      end
+
+      context 'when membership_type is different' do
+        let(:type_copy) { 'admin' }
+
+        it 'is valid' do
+          expect(group_membership).to be_valid
+        end
       end
     end
-
-    # context 'when user get removed from group' do
-    #   it 'has correct error' do
-    #     group.validate
-    #     expect(group.errors.full_messages).to include I18n.t('groups.no_admin_validation')
-    #   end
-    # end
   end
 end
