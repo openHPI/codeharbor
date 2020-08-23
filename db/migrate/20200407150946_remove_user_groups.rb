2745fc24a0e15905b2c430ad94c9e6eab90a5ac6
Karol <git@koehn.pro>
2020-08-23 15:17:37 +0200

    make migration reversible

--- db/migrate/20200407150946_remove_user_groups.rb
+++ db/migrate/20200407150946_remove_user_groups.rb
@@ -1,5 +1,14 @@
 class RemoveUserGroups < ActiveRecord::Migration[6.0]
   def change
-    drop_table :user_groups
+    drop_table :user_groups do |t|
+      t.boolean "is_admin", default: false
+      t.boolean "is_active", default: false
+      t.integer "user_id"
+      t.integer "group_id"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["group_id"], name: "index_user_groups_on_group_id"
+      t.index ["user_id"], name: "index_user_groups_on_user_id"
+    end
   end
 end
