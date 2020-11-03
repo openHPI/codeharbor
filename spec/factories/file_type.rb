130c55f1b99cca7a4aac1d797837e204fbb5b962
Karol <git@koehn.pro>
2020-11-03 17:21:57 +0100

    fix specs

--- spec/factories/file_type.rb
+++ spec/factories/file_type.rb
@@ -4,20 +4,24 @@ FactoryBot.define do
   factory :file_type, class: 'FileType' do
     name { 'Java' }
     file_extension { '.java' }
-  end
 
-  factory :java_file_type, class: 'FileType' do
-    name { 'Java' }
-    file_extension { '.java' }
-  end
+    initialize_with do
+      FileType.find_or_create_by(file_extension: file_extension) do |filetype|
+        filetype.name = name
+      end
+    end
 
-  factory :txt_file_type, class: 'FileType' do
-    name { 'Plain Text' }
-    file_extension { '.txt' }
-  end
+    factory :java_file_type, class: 'FileType' do
+    end
 
-  factory :bmp_file_type, class: 'FileType' do
-    name { 'Bitmap' }
-    file_extension { '.bmp' }
+    factory :txt_file_type, class: 'FileType' do
+      name { 'Plain Text' }
+      file_extension { '.txt' }
+    end
+
+    factory :bmp_file_type, class: 'FileType' do
+      name { 'Bitmap' }
+      file_extension { '.bmp' }
+    end
   end
 end
