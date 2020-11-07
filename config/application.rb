267180a68221996d6c84daec9d9f279112bf2cd1
Karol <git@koehn.pro>
2020-11-07 11:34:21 +0100

    remove last codeharbour occurences

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Codeharbour
+module Codeharbor
   class Application < Rails::Application
     config.load_defaults 6.0
 
