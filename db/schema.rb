5343fb5e84dd151886a96fddb4210186fed57637
Karol <git@koehn.pro>
2020-11-08 11:55:49 +0100

    Add and install activestorage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_150946) do
+ActiveRecord::Schema.define(version: 2020_11_07_103507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -31,6 +31,27 @@ ActiveRecord::Schema.define(version: 2020_04_07_150946) do
     t.string "name"
     t.string "check_uuid_url"
     t.index ["user_id"], name: "index_account_links_on_user_id"
+  end
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "cart_exercises", id: :serial, force: :cascade do |t|
@@ -321,6 +342,7 @@ ActiveRecord::Schema.define(version: 2020_04_07_150946) do
   end
 
   add_foreign_key "account_links", "users"
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "carts", "users"
   add_foreign_key "comments", "exercises"
   add_foreign_key "comments", "users"
