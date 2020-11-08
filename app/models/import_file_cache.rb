03941f853fd9fad9f8453f7cc67d03853b24cc7b
Karol <git@koehn.pro>
2020-11-08 13:28:43 +0100

    Migrate cached import to active storage

--- app/models/import_file_cache.rb
+++ app/models/import_file_cache.rb
@@ -3,6 +3,7 @@
 class ImportFileCache < ApplicationRecord
   belongs_to :user
 
-  has_attached_file :zip_file
-  do_not_validate_attachment_file_type :zip_file
+  # has_attached_file :zip_file
+  has_one_attached :zip_file
+  # do_not_validate_attachment_file_type :zip_file
 end
