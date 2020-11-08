557d9f5cd4588132b502b83f1ff150f99cf4d9d7
Karol <git@koehn.pro>
2020-11-08 17:42:53 +0100

    Migrate avatars to active storage use image-processing gem instead of image magick

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_08_112202) do
+ActiveRecord::Schema.define(version: 2020_11_08_164104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -325,10 +325,6 @@ ActiveRecord::Schema.define(version: 2020_11_08_112202) do
     t.datetime "updated_at", null: false
     t.string "role", default: "user"
     t.boolean "deleted"
-    t.string "avatar_file_name"
-    t.string "avatar_content_type"
-    t.integer "avatar_file_size"
-    t.datetime "avatar_updated_at"
     t.string "username"
     t.text "description"
     t.boolean "email_confirmed", default: false
