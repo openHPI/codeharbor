8a445f1837dcc351fad4948824e2f20901b3f74c
Karol <git@koehn.pro>
2020-11-14 12:33:30 +0100

    last steps to migrate exercise_file attachments to activestorage

--- app/models/user.rb
+++ app/models/user.rb
@@ -158,9 +158,7 @@ class User < ApplicationRecord
 
     avatar_blob = avatar.blob
     if avatar_blob.content_type.start_with? 'image/'
-      if avatar_blob.byte_size > 10.megabytes
-        errors.add(:avatar, 'size needs to be less than 10MB')
-      end
+      errors.add(:avatar, 'size needs to be less than 10MB') if avatar_blob.byte_size > 10.megabytes
     else
       errors.add(:avatar, 'needs to be an image')
     end
