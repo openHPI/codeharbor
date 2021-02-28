7bfc6a3ef9ef2d607286341b8d5b854ea7686d4f
Karol <git@koehn.pro>
2021-02-28 11:42:47 +0100

    review points - remove filetype

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_21_104627) do
+ActiveRecord::Schema.define(version: 2021_02_28_104206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -82,14 +82,6 @@ ActiveRecord::Schema.define(version: 2021_02_21_104627) do
     t.bigint "task_id"
     t.index ["task_id"], name: "index_comments_on_task_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
-  create_table "file_types", id: :serial, force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "file_extension"
-    t.string "editor_mode"
   end
 
   create_table "group_memberships", id: :serial, force: :cascade do |t|
