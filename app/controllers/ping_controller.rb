ead82e40a18f8ca40348eed4ef71ed57986ce2a2
Sebastian Serth <Sebastian.Serth@hpi.de>
2021-09-24 08:57:26 +0200

    Add ping/pong health check

---
+++ app/controllers/ping_controller.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class PingController < ApplicationController
+  before_action :postgres_connected!
+
+  def index
+    render json: {
+      message: 'Pong',
+      timenow_in_time_zone____: DateTime.now.in_time_zone.to_i,
+      timenow_without_timezone: DateTime.now.to_i
+    }
+  end
+
+  private
+
+  def postgres_connected!
+    ApplicationRecord.establish_connection
+    ApplicationRecord.connection
+    ApplicationRecord.connected?
+  end
+end
