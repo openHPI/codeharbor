4044ce478140d100241baed9bd65e4aede35c35b
Sebastian Serth <Sebastian.Serth@hpi.de>
2022-01-10 09:22:56 +0100

    Configure SAML Test ID and BIRD strategies
    * The samltestid certificate is newly generated and can be used for testing purposes with https://samltest.id/. It won't be available in production

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -275,6 +275,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :bird, strategy_class: OmniAuth::Strategies::Bird if Settings.omniauth.bird.enable
+  config.omniauth :samltestid, strategy_class: OmniAuth::Strategies::SamlTestId if Settings.omniauth.samltestid.enable
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
