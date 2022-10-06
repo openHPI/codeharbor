7be909efb6207f8e751ab55fc790e1b7ed5329eb
Karol <git@koehn.pro>
2022-10-06 22:24:34 +0200

    refactor star filter

--- app/models/task.rb
+++ app/models/task.rb
@@ -32,11 +32,12 @@ class Task < ApplicationRecord
   scope :owner, ->(user) { where(user: user) }
   scope :visibility, ->(visibility, user = nil) { {owner: owner(user), public: not_owner(user)}.with_indifferent_access[visibility] }
   scope :created_before_days, ->(days) { where(created_at: days.to_i.days.ago.beginning_of_day..) if days.to_i.positive? }
+  scope :min_stars, ->(stars) { joins('LEFT JOIN (SELECT task_id, AVG(rating) AS avg_rating FROM ratings GROUP BY task_id) AS ratings ON ratings.task_id = tasks.id').where('COALESCE(avg_rating, 0) >= ?', stars) }
 
   serialize :meta_data, HashAsJsonbSerializer
 
   def self.ransackable_scopes(_auth_object = nil)
-    %i[created_before_days]
+    %i[created_before_days min_stars]
   end
 
   def self.ransackable_attributes(_auth_object = nil)
