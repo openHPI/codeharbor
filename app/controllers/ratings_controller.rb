f5586251c7d50704d375e0ed57215f32626e1aa6
Karol <git@koehn.pro>
2022-10-12 20:30:04 +0200

    add specs

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -9,17 +9,15 @@ class RatingsController < ApplicationController
   end
 
   def create
-    # return handle_own_rating if @task.user == current_user
+    return handle_own_rating if @task.user == current_user
 
-    rating = handle_rating
-
-    respond_to do |format|
-      if rating.save
-        overall_rating = @task.average_rating
-        format.json { render json: {overall_rating: overall_rating, user_rating: rating} }
-      else
-        format.json { render layout: false, notice: t('controllers.generic_error') }
-      end
+    rating, notice = handle_rating
+    if rating.save
+      overall_rating = @task.average_rating
+      render json: {overall_rating: overall_rating, user_rating: rating}
+      flash.now[:notice] = notice
+    else
+      render json: {notice: t('controllers.generic_error')}
     end
   end
 
@@ -31,30 +29,21 @@ class RatingsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the following list through.
   def rating_params
-    params.require(:rating).permit(:rating, :task_id, :user_id)
+    params.require(:rating).permit(:rating, :task_id)
   end
 
   def handle_own_rating
     flash.now[:alert] = t('controllers.rating.own_task')
     overall_rating = @task.average_rating
-    respond_to do |format|
-      format.json { render json: {overall_rating: overall_rating, user_rating: overall_rating} }
-    end
+    render json: {overall_rating: overall_rating, user_rating: overall_rating}
   end
 
   def handle_rating
-    rating = @task.ratings.find_by(user: current_user)
-    notice = t('controllers.rating.success.create')
-    if rating
-      notice = t('controllers.rating.success.update')
-      rating.update(rating_params)
-    else
-      rating = Rating.new(rating_params)
-    end
-    rating.task = @task
-    rating.user = current_user
-    flash.now[:notice] = notice
+    rating = @task.ratings.find_or_initialize_by(user: current_user)
+    rating.rating = rating_params[:rating]
 
-    rating
+    notice = rating.persisted? ? t('controllers.rating.success.update') : t('controllers.rating.success.create')
+
+    [rating, notice]
   end
 end
