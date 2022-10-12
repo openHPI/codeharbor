1bac52c80830cc8d18fda763c50cf309e2e7c65b
Karol <git@koehn.pro>
2022-10-12 20:32:56 +0200

    remove wrong spec

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -8,12 +8,4 @@ RSpec.describe Rating do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to validate_numericality_of(:rating).only_integer.is_less_than_or_equal_to(5).is_greater_than(0) }
   end
-
-  describe '#language_with_version' do
-    subject { programming_language.language_with_version }
-
-    let(:rating) { build(:rating) }
-
-    it { is_expected.to eql 'Ruby 3.0.0' }
-  end
 end
