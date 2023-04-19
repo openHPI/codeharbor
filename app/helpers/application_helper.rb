560fcde35b98c20acc5a9c0b28d837d2a110bcca
zuhrasofyan <zuhrasofyan@gmail.com>
2023-04-19 16:11:17 +0200

    Fix the navbar to use Bootstrap 5 components

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  # Add an active class when the current page match with the path (also true for the child page)
+  def active_class(path)
+    current_path = current_page? path
+    current_path || request.path.start_with?(path) ? 'active' : ''
+  end
 end
