5715da5e5b3716af8eefdbf1c55b5988185ed083
Mathis Zscheischler <55900078+Mathis-Z@users.noreply.github.com>
2023-04-21 16:31:22 +0200

    Apply suggestions from code review
    Co-authored-by: Sebastian Serth <MrSerth@users.noreply.github.com>

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -10,19 +10,19 @@ RSpec.describe CommentsController do
 
   before { sign_in user }
 
-  describe 'index' do
+  describe 'GET #index' do
     it 'renders load_comments view' do
-      get :index, xhr: true, format: :js, params: {task_id: task.id}
+      get :index, params: {task_id: task.id}, format: :js, xhr: true
       expect(response).to render_template('load_comments')
     end
 
     it 'answers with HTTP 200 OK' do
-      get :index, xhr: true, format: :js, params: {task_id: task.id}
+      get :index, params: {task_id: task.id}, format: :js, xhr: true
       expect(response).to have_http_status :ok
     end
   end
 
-  describe 'edit' do
+  describe 'GET #edit' do
     it 'answers with HTTP 200 OK' do
       get :index, xhr: true, format: :js, params: {task_id: task.id}
       expect(response).to have_http_status :ok
