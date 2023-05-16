6c6b6b96f7b3b254fa9250891fbc71ad59fe0057
Florian Strehl <florian.strehl@student.hpi.uni-potsdam.de>
2023-05-16 09:36:03 +0200

    Removed remains of rebase-conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.0].define(version: 2023_04_26_101035) do
-=======
 ActiveRecord::Schema[7.0].define(version: 2023_05_01_095629) do
->>>>>>> 92056b9 (Implemented database requirements for task access level)
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
