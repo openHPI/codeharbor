42afb9bacad4f1309fd927d3b85553046efd7555
Mathis-Z <Mathis.Zscheischler@gmail.com>
2023-06-28 10:55:25 +0200

    adding test case for the discussed bug

---
+++ spec/features/tasks_controller_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'TasksController', js: true do
+  let(:user) { create(:user) }
+  let(:password) { attributes_for(:user)[:password] }
+
+  before do
+    sign_in_with_js_driver(user, password)
+  end
+
+  describe '#new' do
+    before { visit(new_task_path) }
+
+    context 'with invalid params' do
+      let(:not_existing_label_name) { 'some new label' }
+
+      it 'displays labels after invalid submission attempt' do
+        input = find('select', id: 'task_label_names').sibling('span', class: 'select2').find('input')
+        input.send_keys(not_existing_label_name)
+        wait_for_ajax
+        input.send_keys(:enter)
+        click_button(I18n.t('tasks.form.save'))
+        expect(page).to have_selector("option[value=\"#{not_existing_label_name}\"][selected=\"selected\"]", visible: :all)
+      end
+    end
+  end
+end
--- spec/support/authentication.rb
+++ spec/support/authentication.rb
@@ -4,4 +4,12 @@ module Authentication
   def sign_in(user, password)
     page.driver.post(user_session_path, user: {email: user.email, password:})
   end
+
+  def sign_in_with_js_driver(user, password)
+    visit(new_user_session_path)
+    fill_in(I18n.t('sessions.email.label'), with: user.email)
+    fill_in(I18n.t('sessions.password.label'), with: password)
+    click_button(I18n.t('sessions.login'))
+    expect(page).to have_content(I18n.t('devise.sessions.signed_in'))
+  end
 end
