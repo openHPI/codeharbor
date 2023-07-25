ffa59038ba1d0ca3fd6f39dc90816e95ea528df6
Florian Strehl <florian.strehl@student.hpi.uni-potsdam.de>
2023-07-25 10:17:21 +0200

    Implemented discard_changes

--- app/models/task.rb
+++ app/models/task.rb
@@ -108,7 +108,7 @@ class Task < ApplicationRecord
   end
 
   def contribution?
-    task_contribution.present?
+    task_contribution.present? && task_contribution.status == :open
   end
 
   def sync_metadata_with_nbp
@@ -130,7 +130,7 @@ class Task < ApplicationRecord
   end
 
   # This method resets all permissions and optionally assigns a useful title
-  def clean_duplicate(user, change_title: true)
+  def clean_duplicate(user, change_title = true)
     duplicate.tap do |task|
       task.user = user
       task.groups = []
