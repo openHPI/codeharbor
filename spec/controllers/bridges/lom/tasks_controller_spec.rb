70a65b1e1409e3fd12fceffffc1a8bfe6c825ebc
Sebastian Serth <Sebastian.Serth@hpi.de>
2023-07-28 16:41:09 +0200

    Increase test coverage for IEEE LOM bridge

--- spec/controllers/bridges/lom/tasks_controller_spec.rb
+++ spec/controllers/bridges/lom/tasks_controller_spec.rb
@@ -6,16 +6,15 @@ RSpec.describe Bridges::Lom::TasksController do
   render_views
 
   let(:user) { create(:user) }
-  let(:programming_language) { create(:programming_language) }
+  let(:programming_language) { create(:programming_language, :ruby) }
   let(:license) { create(:license) }
   let(:parent_uuid) { create(:task).uuid }
-  let(:public_task) { create(:task, :with_content, user:, parent_uuid:, programming_language:, license:, access_level: :public) }
-  let(:private_task) { create(:task, :with_content, user:, parent_uuid:, programming_language:, license:, access_level: :private) }
+  let(:task) { create(:task, :with_content, :with_labels, user:, parent_uuid:, programming_language:, license:, access_level:) }
 
   describe 'GET #show' do
     subject(:get_request) { get :show, params: {id: task.to_param} }
 
-    shared_examples 'is a valid LOM reponse' do
+    shared_examples 'is a valid LOM response' do
       it 'returns HTTP OK' do
         expect(response).to have_http_status :ok
       end
@@ -28,16 +27,14 @@ RSpec.describe Bridges::Lom::TasksController do
     end
 
     context 'without being signed in' do
-      let(:task) { public_task }
-
       context 'when task is public' do
-        let(:task) { public_task }
+        let(:access_level) { :public }
 
-        include_examples 'is a valid LOM reponse'
+        include_examples 'is a valid LOM response'
       end
 
       context 'when task is private' do
-        let(:task) { private_task }
+        let(:access_level) { :private }
 
         it 'returns HTTP forbidden' do
           get_request
@@ -50,15 +47,15 @@ RSpec.describe Bridges::Lom::TasksController do
       before { sign_in user }
 
       context 'when task is public' do
-        let(:task) { public_task }
+        let(:access_level) { :public }
 
-        include_examples 'is a valid LOM reponse'
+        include_examples 'is a valid LOM response'
       end
 
       context 'when task is private' do
-        let(:task) { private_task }
+        let(:access_level) { :private }
 
-        include_examples 'is a valid LOM reponse'
+        include_examples 'is a valid LOM response'
       end
     end
 
@@ -66,13 +63,13 @@ RSpec.describe Bridges::Lom::TasksController do
       before { sign_in create(:user) }
 
       context 'when task is public' do
-        let(:task) { public_task }
+        let(:access_level) { :public }
 
-        include_examples 'is a valid LOM reponse'
+        include_examples 'is a valid LOM response'
       end
 
       context 'when task is private' do
-        let(:task) { private_task }
+        let(:access_level) { :private }
 
         it 'returns HTTP forbidden' do
           get_request
@@ -83,7 +80,7 @@ RSpec.describe Bridges::Lom::TasksController do
 
     context 'when signed in as a group member' do
       let(:group_member) { create(:user) }
-      let(:task) { private_task }
+      let(:access_level) { :private }
       let(:group_memberships) { [build(:group_membership, :with_admin), build(:group_membership, user: group_member)] }
 
       before do
@@ -91,7 +88,29 @@ RSpec.describe Bridges::Lom::TasksController do
         sign_in group_member
       end
 
-      include_examples 'is a valid LOM reponse'
+      include_examples 'is a valid LOM response'
+    end
+
+    context 'with additional details' do
+      let(:access_level) { :public }
+
+      context 'when no license is specified' do
+        let(:license) { nil }
+
+        include_examples 'is a valid LOM response'
+      end
+
+      context 'when ratings are available' do
+        before { create(:rating, task:, user:) }
+
+        include_examples 'is a valid LOM response'
+      end
+
+      context 'when comments are available' do
+        before { create(:comment, task:, user:) }
+
+        include_examples 'is a valid LOM response'
+      end
     end
   end
 end
