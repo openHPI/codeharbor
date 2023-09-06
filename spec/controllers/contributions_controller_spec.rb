a53ab4b02a9e0383f44f761c8192f5ff960aece6
Florian Strehl <florian.strehl@student.hpi.uni-potsdam.de>
2023-09-06 10:01:10 +0200

    WIP

--- spec/controllers/contributions_controller_spec.rb
+++ spec/controllers/contributions_controller_spec.rb
@@ -8,6 +8,8 @@ RSpec.describe ContributionsController do
   let(:user) { create(:user) }
   let(:original_author) { create(:user) }
   let(:task) { create(:task, user: original_author) }
+  let(:contrib_task) { create(:task, user:, title: 'Modified title', parent_uuid: task.uuid) }
+  let(:contribution) { create(:task_contribution, task: contrib_task, status: :pending) }
 
   before { sign_in user }
   describe 'GET #new' do
@@ -47,5 +49,24 @@ RSpec.describe ContributionsController do
         expect(response).to redirect_to(Task.find(assigns(:task).id))
       end
     end
+  end
+
+  describe 'POST #approve_changes' do
+    context 'with valid params' do
+      subject(:request) { post :approve_changes, params: { task_id: task.id, contribution_id: contribution.id } }
+
+      it 'changes the original task, but retains certain fields' do
+        request
+        expect(assigns(:task).id).to eq(task.id)
+        expect(assigns(:task).user).to eq(original_author)
+        expect(assigns(:task).title).to eq('Modified title')
+      end
+
+      it 'changes the contribution status' do
+        request
+        expect(TaskContribution.find(contribution.id).status).to eq('merged')
+      end
+    end
+
   end
 end
