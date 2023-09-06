871963a0dc6c1cf7682595e8945429d347f6e2f6
Florian Strehl <florian.strehl@student.hpi.uni-potsdam.de>
2023-09-06 10:01:10 +0200

    Fixed controller tests + moved code to model

--- spec/controllers/contributions_controller_spec.rb
+++ spec/controllers/contributions_controller_spec.rb
@@ -7,34 +7,39 @@ RSpec.describe ContributionsController do
 
   let(:user) { create(:user) }
   let(:original_author) { create(:user) }
-  let(:task) { create(:task, user: original_author) }
+  let!(:task) { create(:task, user: original_author) }
+  let(:contrib_task) { create(:task, user:, title: 'Modified title', parent_uuid: task.uuid) }
+  let(:contribution) { create(:task_contribution, task: contrib_task, status: :pending) }
 
   before { sign_in user }
+
   describe 'GET #new' do
     it 'assigns a new task as @task' do
-      get :new, params: { task_id: task.id }
+      get :new, params: {task_id: task.id}
       expect(assigns(:task)).to be_a_new(Task)
     end
   end
 
   describe 'POST #create' do
     context 'with valid params' do
-      subject(:request) { post :create, params: { task_id: task.id, task: task_params } }
+      subject(:post_request) { post :create, params: {task_id: task.id, task: task_params} }
+
       let(:task_params) { attributes_for(:task) }
 
-      it 'creates a new Task' do
-        expect { request }.to change(Task, :count).by(1)
-        expect { request }.to change(TaskContribution, :count).by(1)
+      it 'creates a new Task and TaskContribution' do
+        expect { post_request }
+          .to change(Task, :count).by(1)
+          .and change(TaskContribution, :count).by(1)
       end
 
       it 'assigns a newly created task as @task' do
-        request
+        post_request
         expect(assigns(:task)).to be_a(Task)
         expect(assigns(:task)).to be_persisted
       end
 
       it 'changes required parameters' do
-        request
+        post_request
         expect(assigns(:task).access_level).to eq('private')
         expect(assigns(:task).user).to eq(user)
         expect(assigns(:task).parent_uuid).to eq(task.uuid)
@@ -43,8 +48,50 @@ RSpec.describe ContributionsController do
       end
 
       it 'redirects to the created task' do
-        request
-        expect(response).to redirect_to(Task.find(assigns(:task).id))
+        post_request
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(assigns(:task))
+      end
+    end
+  end
+
+  describe 'POST #approve_changes' do
+    context 'with valid params' do
+      subject(:post_request) { post :approve_changes, params: {task_id: task.id, contribution_id: contribution.id} }
+
+      it 'changes the original task, but retains certain fields' do
+        post_request
+        expect(assigns(:task).id).to eq(task.id)
+        expect(assigns(:task).user).to eq(original_author)
+        expect(assigns(:task).title).to eq('Modified title')
+      end
+
+      it 'changes the contribution status' do
+        post_request
+        expect(TaskContribution.find(contribution.id).status).to eq('merged')
+      end
+
+      it 'redirects to the original task' do
+        post_request
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(task)
+      end
+    end
+  end
+
+  describe 'POST #discard_changes' do
+    context 'with valid params' do
+      subject(:post_request) { post :discard_changes, params: {task_id: task.id, contribution_id: contribution.id} }
+
+      it 'changes the contribution status' do
+        post_request
+        expect(TaskContribution.find(contribution.id).status).to eq('closed')
+      end
+
+      it 'redirects to the modified task' do
+        post_request
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(contrib_task)
       end
     end
   end
