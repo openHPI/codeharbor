d5baaa6a2ded1fce41aaceb4434e117616271469
Sebastian Serth <Sebastian.Serth@hpi.de>
2023-09-08 12:18:00 +0200

    Add condition for SAML initializer
    Previously, eager loading the application could fail if not all OmniAuth strategies were configured.

--- lib/omni_auth/strategies/bird.rb
+++ lib/omni_auth/strategies/bird.rb
@@ -7,14 +7,16 @@ require 'ruby-saml'
 module OmniAuth
   module Strategies
     class Bird < OmniAuth::Strategies::AbstractSaml
-      # Use the metadata received from the Identity Provider for initial configuration
-      idp_metadata_parser = OneLogin::RubySaml::IdpMetadataParser.new
-      idp_metadata = idp_metadata_parser.parse_remote_to_hash(Settings.omniauth.bird.metadata_url)
-      configure idp_metadata
+      if Settings.omniauth.bird.enable
+        # Use the metadata received from the Identity Provider for initial configuration
+        idp_metadata_parser = OneLogin::RubySaml::IdpMetadataParser.new
+        idp_metadata = idp_metadata_parser.parse_remote_to_hash(Settings.omniauth.bird.metadata_url)
+        configure idp_metadata
 
-      # Our Service Provider has some configuration options
-      option :certificate, File.read(Settings.omniauth.bird.certificate)
-      option :private_key, File.read(Settings.omniauth.bird.private_key)
+        # Our Service Provider has some configuration options
+        option :certificate, File.read(Settings.omniauth.bird.certificate)
+        option :private_key, File.read(Settings.omniauth.bird.private_key)
+      end
     end
   end
 end
--- lib/omni_auth/strategies/nbp.rb
+++ lib/omni_auth/strategies/nbp.rb
@@ -7,14 +7,16 @@ require 'ruby-saml'
 module OmniAuth
   module Strategies
     class Nbp < OmniAuth::Strategies::AbstractSaml
-      # Use the metadata received from the Identity Provider for initial configuration
-      idp_metadata_parser = OneLogin::RubySaml::IdpMetadataParser.new
-      idp_metadata = idp_metadata_parser.parse_remote_to_hash(Settings.omniauth.nbp.metadata_url)
-      configure idp_metadata
+      if Settings.omniauth.nbp.enable
+        # Use the metadata received from the Identity Provider for initial configuration
+        idp_metadata_parser = OneLogin::RubySaml::IdpMetadataParser.new
+        idp_metadata = idp_metadata_parser.parse_remote_to_hash(Settings.omniauth.nbp.metadata_url)
+        configure idp_metadata
 
-      # Our Service Provider has some configuration options
-      option :certificate, File.read(Settings.omniauth.nbp.certificate)
-      option :private_key, File.read(Settings.omniauth.nbp.private_key)
+        # Our Service Provider has some configuration options
+        option :certificate, File.read(Settings.omniauth.nbp.certificate)
+        option :private_key, File.read(Settings.omniauth.nbp.private_key)
+      end
     end
   end
 end
