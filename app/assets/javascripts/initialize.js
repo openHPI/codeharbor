fb16499fc299e55d33c6bdf25f1754a01e8bd1c8
zuhrasofyan <zuhrasofyan@gmail.com>
2023-09-18 14:45:58 +0200

    Fix for various elements
    - Fix to comply with bootstrap 5 utility classes
    - Adding alt texts for images
    - Move some text to locales
    - Active (selected) states for tabs

--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -1,3 +1,5 @@
-$(document).on('turbolinks:load', function(){
-    $('[data-toggle="tooltip"]').tooltip();
+$(document).on('turbolinks:load', function () {
+  $('[data-bs-toggle="tooltip"]').tooltip();
+  // Ensure that the tab button will be in active (selected) state for each page that has 'option' query (e.g. groups or messages page)
+  $('#' + $('#option').val()).addClass('selected');
 });
