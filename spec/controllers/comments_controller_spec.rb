1148c49b950684d7d1a810629058ffbfca9cadc8
Mathis-Z <Mathis.Zscheischler@gmail.com>
2023-09-24 15:41:57 +0200

    Limiting access to comments of private tasks

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -6,25 +6,80 @@ RSpec.describe CommentsController do
   render_views
 
   let(:user) { create(:user) }
-  let(:task) { create(:task) }
-
-  before { sign_in user }
+  let(:task_owner) { user }
+  let(:access_level) { :private }
+  let(:task) { create(:task, user: task_owner, access_level:) }
 
   describe 'GET #index' do
-    it 'renders load_comments view' do
-      get :index, params: {task_id: task.id}, format: :js, xhr: true
-      expect(response).to render_template('load_comments')
+    subject(:get_request) { get :index, params: {task_id: task.id}, format: :js, xhr: true }
+
+    shared_examples 'error redirect' do
+      it 'redirects to root page' do
+        get_request
+        expect(response).to have_http_status :redirect
+      end
+
+      it 'shows a flash message' do
+        get_request
+        expect(flash[:alert]).to eq I18n.t('controllers.authorization')
+      end
     end
 
-    it 'answers with HTTP 200 OK' do
-      get :index, params: {task_id: task.id}, format: :js, xhr: true
-      expect(response).to have_http_status :ok
+    shared_examples 'successful response' do
+      it 'renders load_comments view' do
+        get_request
+        expect(response).to render_template('load_comments')
+      end
+
+      it 'answers with HTTP 200 OK' do
+        get_request
+        expect(response).to have_http_status :ok
+      end
+    end
+
+    context 'when not signed in' do
+      context 'when task access level is private' do
+        it_behaves_like 'error redirect'
+      end
+
+      context 'when task access level is public' do
+        let(:access_level) { :public }
+
+        it_behaves_like 'successful response'
+      end
+    end
+
+    context 'when signed in' do
+      before { sign_in user }
+
+      context 'when user is owner of task' do
+        it_behaves_like 'successful response'
+      end
+
+      context 'when user is not owner of task' do
+        let(:task_owner) { create(:user) }
+
+        context 'when task is public' do
+          let(:access_level) { :public }
+
+          it_behaves_like 'successful response'
+        end
+
+        context 'when task is private' do
+          it_behaves_like 'error redirect'
+        end
+      end
     end
   end
 
   describe 'GET #edit' do
+    let(:access_level) { :public }
+    let(:comment) { create(:comment, user:, task:) }
+
+    before { sign_in user }
+
     it 'answers with HTTP 200 OK' do
-      get :index, params: {task_id: task.id}, format: :js, xhr: true
+      get :edit, params: {task_id: task.id, id: comment.id}, format: :js, xhr: true
       expect(response).to have_http_status :ok
     end
   end
