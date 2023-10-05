89921d95d9e11254487b62c97acd7e4485bab8b9
Sebastian Serth <Sebastian.Serth@hpi.de>
2023-10-05 23:35:17 +0200

    Add source map for sprockets

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,6 @@
 //= link_tree ../images
 //= link_tree ../fonts
 //= link_directory ../javascripts .js
+//= link_directory ../javascripts .js.map
 //= link_directory ../stylesheets .css
+//= link_directory ../stylesheets .css.map
