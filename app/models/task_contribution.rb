986d352a0c99e6b134616673b671341e59a7e471
Florian Strehl <florian.strehl@student.hpi.uni-potsdam.de>
2023-10-25 11:07:40 +0200

    Renamed task in TaskContribution to indicate modifying direction

--- app/models/task_contribution.rb
+++ app/models/task_contribution.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
 class TaskContribution < ApplicationRecord
-  belongs_to :task
+  belongs_to :modifying_task, class_name: 'Task', inverse_of: :task_contribution, foreign_key: :task_id
 
   enum status: {pending: 0, closed: 1, merged: 2}
 
   def close
     self.status = :closed
     save
+  end
+
+  def base_task
+    task.parent
   end
 end
