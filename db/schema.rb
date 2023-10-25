a32ddaf4d09f1a2571e45d77b02d3f484f4b47e2
Florian Strehl <florian.strehl@student.hpi.uni-potsdam.de>
2023-10-25 11:07:40 +0200

    Renamed task in TaskContribution to indicate modifying direction

--- db/schema.rb
+++ db/schema.rb
@@ -430,8 +430,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_24_144745) do
     t.jsonb "meta_data"
     t.bigint "testing_framework_id"
     t.jsonb "configuration"
-    t.index ["testing_framework_id"], name: "index_tests_on_testing_framework_id"
     t.integer "parent_id", limit: 2
+    t.index ["testing_framework_id"], name: "index_tests_on_testing_framework_id"
   end
 
   create_table "user_identities", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
