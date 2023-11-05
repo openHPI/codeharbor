8e34b691c4aec016b64c2e0a7221fa4b6124bbe9
Sebastian Serth <Sebastian.Serth@hpi.de>
2023-11-05 11:42:40 +0100

    Ignore i18n_tasks from autoload_lib path

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module CodeHarbor
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks templates middleware omni_auth])
+    config.autoload_lib(ignore: %w[assets tasks templates middleware omni_auth i18n_tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
