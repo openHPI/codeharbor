348b97bd448af9592aacedfd70dc75903e749c99
Florian Strehl <florian.strehl@student.hpi.uni-potsdam.de>
2023-11-21 09:50:41 +0100

    Added mailer for contrib  controller

--- app/mailers/task_contribution_mailer.rb
+++ app/mailers/task_contribution_mailer.rb
@@ -1,22 +1,24 @@
 # frozen_string_literal: true
 
 class TaskContributionMailer < ApplicationMailer
-  def send_contribution_request(task_author, task_contrib, contrib_author)
-    @task_author = task_author
+  def contribution_request(task_contrib)
+    @task_author = task_contrib.base_task.user
     @task_contrib = task_contrib
-    @contrib_author = contrib_author
+    @contrib_author = task_contrib.modifying_task.user
     mail(to: @task_author.email, subject: "#{@contrib_author.name} wants to contribute to your Task '#{task_contrib.base_task.title}'")
   end
 
-  def send_approval_info(task_contrib)
+  def approval_info(task_contrib)
     @task_contrib = task_contrib
     @contrib_author = task_contrib.modifying_task.user
-    mail(to: @contrib_author.email, subject: "#{@contrib_author.name} your contribution for '#{task_contrib.base_task.title}' was approved.")
+    mail(to: @contrib_author.email,
+      subject: "#{@contrib_author.name} your contribution for '#{task_contrib.base_task.title}' was approved.")
   end
 
-  def send_rejection_info(task_contrib)
+  def rejection_info(task_contrib)
     @task_contrib = task_contrib
     @contrib_author = task_contrib.modifying_task.user
-    mail(to: @contrib_author.email, subject: "#{@contrib_author.name} your contribution for '#{task_contrib.base_task.title}' was rejected.")
+    mail(to: @contrib_author.email,
+      subject: "#{@contrib_author.name} your contribution for '#{task_contrib.base_task.title}' was rejected.")
   end
 end
