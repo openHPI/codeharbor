94271906c707ac1a0536dad6941a0d43e0d33d95
Florian Strehl <florian.strehl@student.hpi.uni-potsdam.de>
2023-12-13 09:58:10 +0100

    Fixed file creation in contrib

--- app/models/concerns/transfer_values.rb
+++ app/models/concerns/transfer_values.rb
@@ -3,43 +3,42 @@
 module TransferValues
   # rubocop:disable Metrics/AbcSize
   def transfer_linked_files(other)
-    other.files.each do |file|
-      if files.exists?(file.parent_id)
-        old_file = files.find {|f| f.id == file.parent_id } # Required to force searching a copy in memory instead of db
-        old_file.assign_attributes(file.attributes.except('id', 'parent_id', 'created_date', 'fileable_id'))
-      else
-        temp_file = TaskFile.new(file.attributes.except('id').merge(fileable_id: id))
-        files.append(temp_file)
-      end
-    end
     files.each do |file|
       unless other.files.exists?(parent_id: file.id)
         files.delete(file)
+      end
+    end
+    other.files.each do |file|
+      if files.exists?(file.parent_id)
+        old_file = files.find {|f| f.id == file.parent_id } # Required to force searching a copy in memory instead of db
+        old_file.assign_attributes(file.attributes.except('id', 'parent_id', 'created_at', 'fileable_id'))
+      else
+        files.append(file.duplicate(set_parent_id: false))
       end
     end
   end
   # rubocop:enable Metrics/AbcSize
 
   def transfer_contents(other)
-    assign_attributes(other.attributes.except('id', 'parent_id', 'created_date'))
+    assign_attributes(other.attributes.except('id', 'parent_id', 'created_at'))
     transfer_linked_files(other)
   end
 
-  # rubocop:disable Metrics/AbcSize
-  def transfer_multiple(ours, others, parent)
+  def transfer_multiple(targets, others, _parent)
+    # Remove deleted elements
+    targets.each do |target|
+      unless others.exists?(parent_id: target.id)
+        targets.delete(target)
+      end
+    end
+    # Adding new or modified elements
     others.each do |other|
-      if ours.exists?(other.parent_id)
-        ours.find {|our| our.id = other.parent_id }.transfer_contents other
+      if targets.exists?(other.parent_id)
+        targets.find {|target| target.id = other.parent_id }.transfer_contents other
       else
-        ours.append(other.class.name.constantize.new(other.attributes.except('id').merge(parent)))
+        targets.append(other.duplicate(set_parent_id: false))
       end
     end
-    ours.each do |our|
-      unless others.exists?(parent_id: our.id)
-        ours.delete(our)
-      end
-    end
-    ours
+    targets
   end
-  # rubocop:enable Metrics/AbcSize
 end
