b2dce677711599133ab4a1ee2dcc2e51dd7f3fe0
Sebastian Serth <Sebastian.Serth@hpi.de>
2023-12-13 23:32:50 +0100

    Enable German translation and allow switching

---
+++ app/helpers/authenticated_url_helper.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module AuthenticatedUrlHelper
+  class << self
+    def add_query_parameters(url, parameters)
+      parsed_url = URI.parse url
+
+      # Add the given parameters to the query string
+      query_params = CGI.parse(parsed_url.query || '').with_indifferent_access
+      query_params.merge!(parameters)
+
+      # Add the query string back to the URL
+      parsed_url.query = URI.encode_www_form(query_params)
+
+      # Return the full URL
+      parsed_url.to_s
+    rescue URI::InvalidURIError
+      url
+    end
+  end
+end
