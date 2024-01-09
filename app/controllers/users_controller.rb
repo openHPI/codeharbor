34cf97305618269bbdea232b192cd2e03530279e
Mathis-Z <Mathis.Zscheischler@gmail.com>
2024-01-09 21:24:16 +0100

    Removing unnecessary redirect in UserController
    The redirect is already performed by before_action require_user! in the ApplicationController.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,13 +12,9 @@ class UsersController < ApplicationController
   end
 
   rescue_from ActiveRecord::RecordNotFound do |_exception|
-    if current_user
-      # leak no information whether a user exists or not if the accessing user is not an admin
-      redirect_to({id: current_user.id},
-        alert: current_user.role == 'admin' ? t('common.errors.not_found_error') : t('common.errors.not_authorized'))
-    else
-      redirect_to root_path, alert: t('common.errors.not_authorized')
-    end
+    # leak no information whether a user exists or not if the accessing user is not an admin
+    redirect_to({id: current_user.id},
+      alert: current_user.role == 'admin' ? t('common.errors.not_found_error') : t('common.errors.not_authorized'))
   end
 
   def show; end
