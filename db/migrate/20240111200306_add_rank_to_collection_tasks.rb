aa1ad6f3b04e4b4654b2fb11344ff8bf27e56784
Mathis-Z <Mathis.Zscheischler@gmail.com>
2024-02-05 18:25:30 +0100

    Merging CollectionTask migrations

--- db/migrate/20240111200306_add_rank_to_collection_tasks.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddRankToCollectionTasks < ActiveRecord::Migration[7.1]
-  def change
-    add_column :collection_tasks, :rank, :integer, default: 0, null: false
-  end
-end
--- db/migrate/20240201100955_add_timestamps_to_collection_tasks.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddTimestampsToCollectionTasks < ActiveRecord::Migration[7.1]
-  def change
-    add_timestamps(:collection_tasks, default: -> { 'CURRENT_TIMESTAMP' })
-  end
-end
