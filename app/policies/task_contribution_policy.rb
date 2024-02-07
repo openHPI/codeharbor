13f0a4bd16061ab70bc741cfe7fe81ad65cbcd77
Florian Strehl <florian.strehl@student.hpi.uni-potsdam.de>
2024-02-07 11:08:16 +0100

    Moved to task_contrib pundit

---
+++ app/policies/task_contribution_policy.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+class TaskContributionPolicy < ApplicationPolicy
+  def task_contribution
+    @record
+  end
+
+  def modifying_task
+    @record.modifying_task
+  end
+
+  def base_task
+    @record.base_task
+  end
+
+  %i[create? new?].each do |action|
+    define_method(action) do
+      Pundit.policy(@user, base_task).show? && !Pundit.policy(@user, base_task).edit? && base_task.contributions.where(user: @user).none?
+    end
+  end
+
+  %i[show? discard_changes?].each do |action|
+    define_method(action) do
+      record_owner? || (Pundit.policy(@user, base_task).edit? && task_contribution.pending?)
+    end
+  end
+
+  def approve_changes?
+    Pundit.policy(@user, base_task).edit? && task_contribution.pending?
+  end
+
+  %i[update? destroy?].each do |action|
+    define_method(action) do
+      record_owner?
+    end
+  end
+
+  private
+
+  def record_owner?
+    @user.present? && @user == modifying_task.user
+  end
+end
