4b3384add2d367dd13812585eb8c26de7cd20f42
Mathis-Z <Mathis.Zscheischler@gmail.com>
2024-02-28 10:29:30 +0100

    Storing preferred user locale in database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -97,10 +97,35 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def switch_locale(&) # rubocop:disable Metrics/AbcSize
-    session[:locale] = sanitize_locale(params[:custom_locale] || params[:locale] || session[:locale])
-    locale = session[:locale] || http_accept_language.compatible_language_from(I18n.available_locales) || I18n.default_locale
+  def sanitized_locale_param
+    sanitize_locale(params[:locale])
+  end
+
+  def sanitized_session_locale
+    sanitize_locale(session[:locale])
+  end
+
+  def sanitized_user_preferred_locale
+    return if current_user.nil?
+
+    sanitize_locale(current_user.preferred_locale)
+  end
+
+  def choose_locale
+    sanitized_locale_param ||
+      sanitized_session_locale ||
+      sanitized_user_preferred_locale ||
+      http_accept_language.compatible_language_from(I18n.available_locales) ||
+      I18n.default_locale
+  end
+
+  def switch_locale(&)
+    locale = choose_locale
+    session[:locale] = locale
     Sentry.set_extras(locale:)
+    if current_user.present? && locale != sanitized_user_preferred_locale
+      current_user.update(preferred_locale: locale)
+    end
     I18n.with_locale(locale, &)
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_11_200306) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_21_163906) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -315,6 +315,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_11_200306) do
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
     t.datetime "locked_at"
+    t.string "preferred_locale"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
