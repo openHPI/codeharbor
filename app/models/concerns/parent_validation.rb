9e331671e1c9ec34c9fae4b0e89f3cb8e40a5875
Florian Strehl <florian.strehl@student.hpi.uni-potsdam.de>
2024-03-11 13:42:37 +0100

    Fixed ParentValidation

--- app/models/concerns/parent_validation.rb
+++ app/models/concerns/parent_validation.rb
@@ -2,16 +2,10 @@
 
 module ParentValidation
   def parent_validation_check
-    return
-    if parent_id.nil?
-      return
-    end
+    return if parent_id.nil?
 
-    parent_entry = self.class.find(parent_id)
-    if parent_entry.blank?
-      errors.add(:parent_id, :invalid)
-    end
-    unless parent_entry.task.parent_of?(task)
+    parent_entry = self.class.find_by(id: parent_id)
+    if parent_entry.blank? || !parent_entry.task.parent_of?(task)
       errors.add(:parent_id, :invalid)
     end
   end
---
+++ spec/support/shared_examples/parent_validation.rb
@@ -0,0 +1,47 @@
+# frozen_string_literals: true
+
+RSpec.shared_examples 'parent validation with parent_id' do |model|
+  let(:task) { create(:task) }
+  let(:p_uuid) { nil }
+  let(:other_task) { create(:task, parent_uuid: p_uuid) }
+  let(:original_instance) { create(model, task:) }
+  let(:other_instance) { create(model, task: other_task) }
+
+  context 'when it has no parent' do
+    it 'is valid' do
+      expect(original_instance).to be_valid
+    end
+  end
+
+  context 'when it has a parent' do
+    context 'when the parent_id is invalid' do
+      before do
+        other_instance.parent_id = -1
+      end
+
+      it 'is not valid' do
+        expect(other_instance).not_to be_valid
+      end
+    end
+
+    context 'when the parent_id is valid' do
+      before do
+        other_instance.parent = original_instance
+      end
+
+      context 'when the task has a valid parent' do
+        let(:p_uuid) { task.uuid }
+
+        it 'is valid' do
+          expect(other_instance).to be_valid
+        end
+      end
+
+      context 'when the task has an invalid parent' do
+        it 'is not valid' do
+          expect(other_instance).not_to be_valid
+        end
+      end
+    end
+  end
+end
