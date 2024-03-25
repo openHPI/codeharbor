5fa0d4f82e7a6f4c4428d8bfd2beb9cbb6e257fe
Sebastian Serth <Sebastian.Serth@hpi.de>
2024-03-25 14:25:48 +0100

    Allow adding and removing OmniAuth account links

--- spec/features/rails_admin_spec.rb
+++ spec/features/rails_admin_spec.rb
@@ -6,15 +6,6 @@ RSpec.describe 'RailsAdmin' do
   let(:user) { create(role) }
   let(:password) { attributes_for(role)[:password] }
 
-  before do
-    # RailsAdmin will automatically eager load all models (and some further classes).
-    # Since this eager loading is not done as expected, the OmniAuth strategies
-    # dynamically configuring themselves with an URL will fail and raise an error.
-    # To prevent this, we remove the OmniAuth constant from the global namespace,
-    # so that the strategies are not loaded and the error is not raised.
-    Object.send(:remove_const, :OmniAuth) if defined?(OmniAuth)
-  end
-
   context 'when signed out' do
     it 'redirects to the root page' do
       visit(rails_admin.dashboard_path)
