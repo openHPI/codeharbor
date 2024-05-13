719fd3dd97bc07038c2ef44e712ae951cbe6649e
Florian Strehl <florian.strehl@student.hpi.uni-potsdam.de>
2024-05-13 09:44:33 +0200

    Added foreign key for parent_id

---
+++ db/migrate/20240513071837_parent_id_to_foreign_key.rb
@@ -0,0 +1,7 @@
+class ParentIdToForeignKey < ActiveRecord::Migration[7.1]
+  def change
+    add_foreign_key :model_solutions, :model_solutions, column: :parent_id, on_delete: :nullify
+    add_foreign_key :tests, :tests, column: :parent_id, on_delete: :nullify
+    add_foreign_key :task_files, :task_files, column: :parent_id, on_delete: :nullify
+  end
+end
