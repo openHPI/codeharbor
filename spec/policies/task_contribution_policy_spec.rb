3c99c0c2b01fe2b80d95f38dfe6d3d356835c381
Florian Strehl <florian.strehl@student.hpi.uni-potsdam.de>
2024-05-28 07:59:35 +0200

    Fixed permissions for task contrib

--- spec/policies/task_contribution_policy_spec.rb
+++ spec/policies/task_contribution_policy_spec.rb
@@ -10,7 +10,8 @@ RSpec.describe TaskContributionPolicy do
   let(:new_user) { create(:user) }
   let(:access_level) { 'public' }
   let(:org_task) { create(:task, user: org_user, access_level:) }
-  let(:contribution) { build(:task_contribution, user: new_user, base: org_task) }
+  let(:contribution_approval_status) { 'pending' }
+  let(:contribution) { create(:task_contribution, user: new_user, base: org_task, status: contribution_approval_status) }
 
   context 'when the user is not logged in' do
     it { expect { described_class.new(nil, contribution) }.to raise_error(Pundit::NotAuthorizedError) }
@@ -23,32 +24,42 @@ RSpec.describe TaskContributionPolicy do
       context 'when the original task is private' do
         let(:access_level) { 'private' }
 
-        it { is_expected.to permit_only_actions %i[destroy discard_changes show update edit] } # TODO: This currently tests the case where a public task with a contrib becomes private. We should evaluate in the future what should happen in this case.
+        it { is_expected.to permit_only_actions %i[discard_changes show update edit] } # TODO: This currently tests the case where a public task with a contrib becomes private. We should evaluate in the future what should happen in this case.
       end
 
       context 'when the original task is public' do
         context 'when the user has no contribution for task yet' do
-          it { is_expected.to permit_only_actions %i[create new destroy discard_changes show update edit] }
+          let(:contribution_approval_status) { 'closed' }
+
+          it { is_expected.to permit_only_actions %i[create new] }
         end
 
         context 'when the user has a contribution for the task' do
-          let(:contribution) { create(:task_contribution, user: new_user, base: org_task) }
-
           it { is_expected.to forbid_actions %i[new create approve_changes] }
-          it { is_expected.to permit_actions %i[destroy discard_changes show update] }
+          it { is_expected.to permit_actions %i[discard_changes show update] }
+
+          context 'when the approval is closed' do # rubocop:disable RSpec/NestedGroups
+            let(:contribution_approval_status) { 'closed' }
+
+            it { is_expected.to forbid_actions %i[discard_changes show edit update] }
+          end
         end
       end
     end
 
     context 'when the user own the original task' do
       let(:user) { org_user }
-      let(:contribution_approval_status) { 'pending' }
-      let(:contribution) { create(:task_contribution, user: new_user, base: org_task, status: contribution_approval_status) }
 
       it { is_expected.to forbid_actions %i[create new update destroy] }
 
       context 'when the approval is pending' do
         it { is_expected.to permit_only_actions %i[approve_changes discard_changes show] }
+      end
+
+      context 'when the approval is closed' do
+        let(:contribution_approval_status) { 'closed' }
+
+        it { is_expected.to permit_only_actions %i[show] }
       end
     end
   end
