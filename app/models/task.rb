c308ad782e9292bb9bdd9c357d732ec9be2c9d43
Florian Strehl <florian.strehl@student.hpi.uni-potsdam.de>
2024-05-29 09:00:30 +0200

    Corrected contrib closing workflow

--- app/models/task.rb
+++ app/models/task.rb
@@ -67,7 +67,9 @@ class Task < ApplicationRecord
   scope :not_task_contributions, lambda {
     where.missing(:task_contribution)
   }
-  scope :pending_contribution, ->(user) { joins(:task_contribution).where(user:, access_level: :pending) }
+  scope :pending_contribution, lambda {|user|
+    joins(:task_contribution).where(user:, task_contribution: {status: :pending})
+  }
   scope :created_before_days, ->(days) { where(created_at: days.to_i.days.ago.beginning_of_day..) if days.to_i.positive? }
   scope :min_stars, lambda {|stars|
                       joins('LEFT JOIN (SELECT task_id, AVG(overall_rating) AS avg_overall_rating FROM ratings GROUP BY task_id)
