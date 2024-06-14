5d4dda3092bd837a2a67b1e971bc62f24a2a963b
Sebastian Serth <Sebastian.Serth@hpi.de>
2024-06-14 18:16:43 +0200

    AuthenticatedUrlHelper: Prevent trailing ? without parameters

--- app/helpers/authenticated_url_helper.rb
+++ app/helpers/authenticated_url_helper.rb
@@ -10,7 +10,7 @@ module AuthenticatedUrlHelper
       query_params.merge!(parameters)
 
       # Add the query string back to the URL
-      parsed_url.query = URI.encode_www_form(query_params)
+      parsed_url.query = URI.encode_www_form(query_params).presence
 
       # Return the full URL
       parsed_url.to_s
