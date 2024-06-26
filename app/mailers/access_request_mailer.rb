5823878ee69fcc3e2c1e4b996989d6a45c71b6a8
Florian Strehl <florian.strehl@student.hpi.uni-potsdam.de>
2024-06-26 08:30:06 +0200

    Moved mailer tests + translations

--- app/mailers/access_request_mailer.rb
+++ app/mailers/access_request_mailer.rb
@@ -9,13 +9,4 @@ class AccessRequestMailer < ApplicationMailer
       mail(to: @admin.email, subject: t('groups.access_request_mailer.subject', user: @user.name, group: @group.name))
     end
   end
-
-  def send_contribution_request(author, exercise, user)
-    @author = author
-    @exercise = exercise
-    @user = user
-    I18n.with_locale(@author.preferred_locale || I18n.default_locale) do
-      mail(to: @author.email, subject: t('tasks.access_request_mailer.subject', user: @user.name, task: @exercise.title))
-    end
-  end
 end
