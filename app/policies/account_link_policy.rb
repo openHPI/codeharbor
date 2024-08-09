f17547c404a019725360e49df6ed31270e5d2a17
Karol <git@koehn.pro>
2024-08-09 17:46:53 +0200

    authorize usage of account_link

--- app/policies/account_link_policy.rb
+++ app/policies/account_link_policy.rb
@@ -13,7 +13,7 @@ class AccountLinkPolicy < ApplicationPolicy
     define_method(action) { admin? || record_owner? }
   end
 
-  %i[show? remove_shared_user?].each do |action|
+  %i[show? remove_shared_user? use?].each do |action|
     define_method(action) { admin? || record_owner? || account_link.shared_users.include?(@user) }
   end
 end
