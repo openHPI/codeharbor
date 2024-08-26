4ad3ca4c3252a30a7d41e57310e76a549a9d4268
Sebastian Serth <Sebastian.Serth@hpi.de>
2024-08-26 15:25:41 +0200

    Overwrite Label#to_s to ease debugging
    Since a label is considered "unique" based on its name, overwriting `to_s` makes sense to aid debugging.

--- spec/models/label_spec.rb
+++ spec/models/label_spec.rb
@@ -6,4 +6,13 @@ RSpec.describe Label do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
   end
+
+  describe '#to_s' do
+    let(:label_name) { 'Test Label' }
+    let(:label) { described_class.new(name: label_name) }
+
+    it 'returns the label name' do
+      expect(label.to_s).to eq label_name
+    end
+  end
 end
