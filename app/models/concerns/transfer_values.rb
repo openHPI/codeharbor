2acc63cc94d23544a60bb86bbe89b94a76a3f3df
Sebastian Serth <Sebastian.Serth@hpi.de>
2024-09-01 20:47:58 +0200

    Refactor exclusion of attributes during transfer
    * Make method private
    * Reduce parameters and avoid passing strings of class names
    * Overwrite for file specifically

--- app/models/concerns/transfer_values.rb
+++ app/models/concerns/transfer_values.rb
@@ -1,27 +1,18 @@
 # frozen_string_literal: true
 
 module TransferValues
-  def excluded_attributes(entity_type)
-    exclude = %w[id parent_id created_at task_id]
-    if entity_type == 'task_file'
-      exclude.append('fileable_id', 'fileable_type')
-    else
-      exclude.append('xml_id')
-    end
-    exclude
-  end
+  def transfer_attributes(other) # rubocop:disable Metrics/AbcSize
+    assign_attributes(other.attributes.except(*excluded_attributes_from_transfer))
 
-  def transfer_attributes(other, excluded_attributes = [])
-    assign_attributes(other.attributes.except(*excluded_attributes))
     if is_a?(TaskFile) && other.attachment.attached?
       attachment.attach(other.attachment.blob)
       self.use_attached_file = 'true'
     elsif !is_a?(TaskFile)
-      transfer_multiple_entities(files, other.files, 'task_file')
+      transfer_multiple_entities(files, other.files)
     end
   end
 
-  def transfer_multiple_entities(targets, others, entity_type) # rubocop:disable Metrics/AbcSize
+  def transfer_multiple_entities(targets, others)
     # Remove deleted elements
     targets.each do |target|
       unless others.exists?(parent_id: target.id)
@@ -32,10 +23,22 @@ module TransferValues
     others.to_a.each do |other|
       if targets.exists?(other.parent_id)
         old_entity = targets.find {|target| target.id == other.parent_id } # Required to force searching a copy in memory instead of db
-        old_entity.transfer_attributes(other, excluded_attributes(entity_type))
+        old_entity.transfer_attributes(other)
       else
         targets.append(other.duplicate(set_parent_id: false))
       end
     end
+  end
+
+  private
+
+  def excluded_attributes_from_transfer
+    exclude = %w[id parent_id created_at task_id]
+    if is_a?(TaskFile)
+      exclude.append('fileable_id', 'fileable_type')
+    else
+      exclude.append('xml_id')
+    end
+    exclude
   end
 end
