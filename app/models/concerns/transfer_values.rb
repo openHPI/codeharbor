40dff8b504f61c1cff3f25a9120e9dc4ff209827
Sebastian Serth <Sebastian.Serth@hpi.de>
2024-09-01 20:37:17 +0200

    Correctly apply changes to attached files when accepting a contribution

--- app/models/concerns/transfer_values.rb
+++ app/models/concerns/transfer_values.rb
@@ -11,9 +11,12 @@ module TransferValues
     exclude
   end
 
-  def transfer_attributes(other, excluded_attributes = [], has_files: true)
+  def transfer_attributes(other, excluded_attributes = [])
     assign_attributes(other.attributes.except(*excluded_attributes))
-    if has_files
+    if is_a?(TaskFile) && other.attachment.attached?
+      attachment.attach(other.attachment.blob)
+      self.use_attached_file = 'true'
+    elsif !is_a?(TaskFile)
       transfer_multiple_entities(files, other.files, 'task_file')
     end
   end
@@ -29,7 +32,7 @@ module TransferValues
     others.to_a.each do |other|
       if targets.exists?(other.parent_id)
         old_entity = targets.find {|target| target.id == other.parent_id } # Required to force searching a copy in memory instead of db
-        old_entity.transfer_attributes(other, excluded_attributes(entity_type), has_files: entity_type != 'task_file')
+        old_entity.transfer_attributes(other, excluded_attributes(entity_type))
       else
         targets.append(other.duplicate(set_parent_id: false))
       end
