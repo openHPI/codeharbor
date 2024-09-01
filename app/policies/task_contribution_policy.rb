26024609c269d67475067dae99bfb103a89ad064
Sebastian Serth <Sebastian.Serth@hpi.de>
2024-09-01 16:31:11 +0200

    Allow admins to perform any action on contributions

--- app/policies/task_contribution_policy.rb
+++ app/policies/task_contribution_policy.rb
@@ -19,27 +19,33 @@ class TaskContributionPolicy < ApplicationPolicy
     end
   end
 
-  # While discard_changes? and show? look similar, the difference in the position of parentheses is relevant
-  # discard_changes? can only be executed while the task_contribution is pending by either the owner of the base task or the contrib author
-  # show? allows the owner of the base task to view it anytime.
-  # Simultaneously, the contributor can view it only while the contrib is still pending
   def discard_changes?
-    task_contribution.pending? && record_owner?
+    return no_one unless task_contribution.pending?
+
+    record_owner? || admin?
   end
 
   def show?
-    (task_contribution.pending? && record_owner?) || Pundit.policy(@user, base).edit?
+    # show? allows the owner of the base task to view it anytime.
+    # Simultaneously, the contributor can view it only while the contrib is still pending
+    return true if Pundit.policy(@user, base).edit?
+
+    task_contribution.pending? && record_owner?
   end
 
   %i[approve_changes? reject_changes?].each do |action|
     define_method(action) do
-      Pundit.policy(@user, base).edit? && task_contribution.pending?
+      return no_one unless task_contribution.pending?
+
+      Pundit.policy(@user, base).edit?
     end
   end
 
   %i[edit? update?].each do |action|
     define_method(action) do
-      record_owner? && task_contribution.pending?
+      return no_one unless task_contribution.pending?
+
+      record_owner? || admin?
     end
   end
 
