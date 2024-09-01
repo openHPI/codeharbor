ffe66c71730e7bbac87fec2a095985a67695beef
Sebastian Serth <Sebastian.Serth@hpi.de>
2024-09-01 19:54:09 +0200

    Allow new task contributions without changes to attached files

--- app/controllers/concerns/task_parameters.rb
+++ app/controllers/concerns/task_parameters.rb
@@ -5,7 +5,7 @@ module TaskParameters
 
   def file_params
     %i[id content attachment path name internal_description mime_type use_attached_file used_by_grader visible usage_by_lms xml_id _destroy
-       parent_id]
+       parent_id parent_blob_id]
   end
 
   def test_params
