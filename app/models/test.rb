7cc7bd71a8a1b7ad3d5605288825a645297b67f7
Sebastian Serth <Sebastian.Serth@hpi.de>
2024-09-11 19:26:54 +0200

    Allow moving ProFormA files between fileables
    We recently discovered an edge case where a moved file was not updated correctly. At first, it got removed and recreated, and later on only removed. While these changes look pretty difficult, they are the best and only working approach I could find to fix the problem ;). For now, I'd suggest to go with this solution. See https://github.com/openHPI/codeharbor/pull/1606 for more details.

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
-  belongs_to :task
+  include FileConcern
+  belongs_to :task, autosave: true, inverse_of: :tests
   belongs_to :testing_framework, optional: true
-  has_many :files, as: :fileable, class_name: 'TaskFile', dependent: :destroy
-  accepts_nested_attributes_for :files, allow_destroy: true
   validates :title, presence: true
   validates :xml_id, presence: true
   validates :xml_id, uniqueness: {scope: :task_id}
