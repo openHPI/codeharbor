b9454a84d4659e9ff7b5b5a9143a102344c78e93
Nele Noack <nele.noack@hpi.de>
2025-02-05 12:39:26 +0100

    refactor(enmeshed): move serialization for creating IdentityAttributes to `Attribute::Identity`
    The former implementation of `Attibute.to_json` was very specific to IdentityAttributes.
    It actually only worked for simple IdentityAttributes and would need adaptations for complex
    IdentityAttributes.
    
    Part of XI-6523

--- lib/enmeshed/attribute/identity.rb
+++ lib/enmeshed/attribute/identity.rb
@@ -2,6 +2,21 @@
 
 module Enmeshed
   class Attribute::Identity < Attribute
+    # Serialize the Identity object to fit the content requirements when creating
+    # an IdentityAttribute via the Connector API (available under Connector::CONNECTOR_URL)
+    #
+    # @return [String] JSON
+    def to_json(*)
+      {
+        content: {
+          value: {
+            '@type': @type,
+            value: @value,
+          },
+        },
+      }.to_json(*)
+    end
+
     private
 
     def additional_attributes
