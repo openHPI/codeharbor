a993cf499567b0ecb8527b544037de4774e3986c
Sebastian Serth <Sebastian.Serth@hpi.de>
2025-02-14 16:42:27 +0100

    Fix Rubocop offenses

--- app/helpers/will_paginate_helper.rb
+++ app/helpers/will_paginate_helper.rb
@@ -17,7 +17,7 @@ module WillPaginateHelper
       end
 
       @template.link_to(target, attributes.merge(remote: true)) do # rubocop:disable Rails/HelperInstanceVariable
-        text.to_s.html_safe # rubocop:disable Rails/OutputSafety as the string is already safe and provided by WillPaginate
+        text.to_s.html_safe # rubocop:disable Rails/OutputSafety -- the string is already safe and provided by WillPaginate
       end
     end
   end
