3accb03b67c96a38dca3749a1f3e860ca6650d4c
Sebastian Serth <Sebastian.Serth@hpi.de>
2025-06-26 10:01:47 +0200

    Move initialization of tooltips to app/javascript
    With this new method, Tooltips are correctly initialized and potential loading errors/race conditions are prevented.

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -55,9 +55,6 @@ $(document).on('turbo-migration:load', function () {
     });
   }
 
-  // Enable all tooltips
-  $('[data-bs-toggle="tooltip"]').tooltip();
-
   // Ensure that the tab button will be in active (selected) state for each page that has 'option' query (e.g. groups or messages page)
   $('#' + $('#option').val()).addClass('selected');
 });
